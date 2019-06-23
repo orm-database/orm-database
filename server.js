04d9cdc26ec23c320303b23961f696deff8083b0
Burke <burke1791@gmail.com>
2019-06-23 10:38:15 -0500

    commented out the root get route

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use(express.json());
 app.listen(port);
 
 //@TODO Delete below after you verify the the app is working
-app.route('/').get(function(request, response) {
-    response.json(config);
-});
+// app.route('/').get(function(request, response) {
+//     response.json(config);
+// });
