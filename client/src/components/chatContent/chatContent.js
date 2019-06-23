fb32d9b010ec48f220ad2b8e86fcc3ce6c9b52b3
Burke <burke1791@gmail.com>
2019-06-23 00:42:32 -0500

    added ChatContent component

---
+++ client/src/components/chatContent/chatContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './chatContent.css';
+
+function ChatContent(props) {
+  return (
+    <div className='col-lg-10 chat-content'>
+      <p>Chat</p>
+    </div>
+  );
+}
+
+export default ChatContent;
