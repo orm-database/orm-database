b25ec624fa0c80e87434efb564599ed866bc7de2
Burke <burke1791@gmail.com>
2019-06-23 00:02:16 -0500

    created the Main component and pseudocoded the blueprint

---
+++ client/src/components/main/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.css';
+
+function Main() {
+  return (
+    <div className='main-content'>
+      {/* GroupsView - left side, full height */}
+
+      {/* ChatContent - rest of the viewport */}
+    </div>
+  );
+}
+
+export default Main;
