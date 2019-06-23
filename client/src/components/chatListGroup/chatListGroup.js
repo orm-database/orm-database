82432915d2987105076a4d5b784c216dde8d7810
Burke <burke1791@gmail.com>
2019-06-23 08:24:02 -0500

    added logic to display different types of chat list items based on the data available

--- client/src/components/chatListGroup/chatListGroup.js
+++ client/src/components/chatListGroup/chatListGroup.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './chatListGroup.css';
 
+import ChatListItem from '../chatListItem/chatListItem';
+
+import { CHAT_GROUP_TYPES } from '../../utilities/constants';
+
 function ChatListGroup(props) {
+  
+  const [groupsFetched, setGroupsFetched] = useState(false);
+  const [groups, setGroups] = useState(null);
+
+  useEffect(() => {
+    // @TODO grab groups from global state
+  }, []);
+
+  const generateChatListItems = () => {
+    // display a loading notification while the code is fetching the groups
+    if (!groupsFetched) {
+      return (
+        <ChatListItem type={CHAT_GROUP_TYPES.loading} />
+      )
+    } else {
+      let groupTypeVals = Object.values(CHAT_GROUP_TYPES);
+
+      // check if groups contains any info - return null if not
+      if (groups) {
+        const items = groups.map(item => {
+          let groupType = groupTypeVals.indexOf(item.type);
+  
+          if (groupType >= 0) {
+            return (
+              <ChatListItem type={groupTypeVals[groupType]} key={item.group_id} />
+            )
+          } else {
+            return null;
+          }
+        });
+  
+        return (items);
+      } else {
+        // display an item indicating that the user is not part of any groups/conversations
+        return (
+          <ChatListItem type={CHAT_GROUP_TYPES.empty} />
+        );
+      }
+    }
+  }
+  
   return (
     <div className="list-group-flush p-0">
-      <button type='button' className="list-group-item list-group-item-action py-1 d-flex justify-content-between align-items-center">
-        Cras justo odio
-        <span className="badge badge-primary badge-pill">14</span>
-      </button>
-      <button type='buton' className="list-group-item list-group-item-action py-1 d-flex justify-content-between align-items-center">
-        Dapibus ac facilisis in
-        <span className="badge badge-primary badge-pill">2</span>
-      </button>
-      <button type='button' className="list-group-item list-group-item-action py-1 d-flex justify-content-between align-items-center">
-        Morbi leo risus
-        <span className="badge badge-primary badge-pill">1</span>
-      </button>
+      {generateChatListItems()}
     </div>
   );
 }
