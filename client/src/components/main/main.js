d2a68be4f3022192a658b7d355861bfa766a868b
Burke <burke1791@gmail.com>
2019-06-23 00:40:57 -0500

    added handleGroupChange function

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './main.css';
 
 import GroupsView from '../groupsView/groupsView';
+import ChatContent from '../chatContent/chatContent';
 
 function Main() {
 
   const [selectedGroup, setSelectedGroup] = useState(null);
 
+  const handleGroupChange = (newGroup) => {
+    setSelectedGroup(newGroup);
+  }
+
   return (
     <div className='container-fluid'>
       <div className='row'>
-        <GroupsView group={selectedGroup} />
-
-        {/* ChatContent - rest of the viewport */}
+        <GroupsView group={selectedGroup} onGroupChange={handleGroupChange} />
+        <ChatContent group={selectedGroup} />
       </div>
     </div>
   );
