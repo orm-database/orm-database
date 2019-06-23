7d1fa0136efb7e9b7ccada1c6c64e04bb0a9d602
Burke <burke1791@gmail.com>
2019-06-22 23:11:57 -0500

    renamed header component to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       {/* Main */}
     </div>
   );
