da442a0bf20822a620661c1852d987d293ee9ce9
Burke <burke1791@gmail.com>
2019-06-22 23:57:09 -0500

    imported the Main component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/navbar/navbar';
+import Main from './components/main/main';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* Main */}
+      <Main />
     </div>
   );
 }
