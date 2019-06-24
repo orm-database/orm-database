98049d4f6e2e7c627c1c31ebdac4aa2e9f072259
Chris Burke <burkcules@Chriss-iMac.local>
2019-06-24 14:56:12 -0500

    added a pubsub object

---
+++ client/src/utilities/pubsub.js
@@ -0,0 +1,40 @@
+var PubSub = {};
+
+(function(obj) {
+  obj.publish = (notif, data) => {
+    if (!obs[notif]) {
+      return false;
+    }
+
+    let subs = obs[notif];
+
+    for (var sub of subs) {
+      sub.callback(data);
+    }
+  };
+
+  obj.subscribe = (notif, sub, cb) => {
+    if (!obs[notif]) {
+      obs[notif] = [];
+    }
+
+    obs[notif].push({
+      observer: sub,
+      callback: cb
+    });
+  };
+
+  obj.unsubscribe = (notif, sub) => {
+    let subs = obs[notif];
+
+    for (var i in subs) {
+      if (subs[i].observer === sub) {
+        subs.splice(i, 1);
+        obs[notif] = subs;
+        return;
+      }
+    }
+  };
+})(PubSub);
+
+export default PubSub;
