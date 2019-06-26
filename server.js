c4e66ba07dfa0df197a3de70456e78b5020a68c8
Daniel Sochor <danielsochor@gmail.com>
2019-06-25 20:14:22 -0500

    Updates for socket.io intergration

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
 require('dotenv').config();
 
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const express = require('express');
+const socketIO = require('socket.io');
+const app = express()
 
-io.on('connection', () =>{
-    console.log('a user is connected');
-})
+const server = express()
+  .listen(port, () => console.log(`Listening on ${ port }`));
 
-//io.on('connection', function (socket) {
-  //  console.log('a user connected');
-    //socket.emit('news', {hello: 'world'});
-    //socket.on('my other event', function (data){
-    //    console.log(data);
-    //});
-//});
-
-const path = require('path');
-const env = process.env.NODE_ENV || 'development';
-const reactConfig = require(path.join(__dirname, '/config/config.static.json'))[env];
+const io = socketIO(server);
 
 // Default port cannot be 3000
 // If you change it from 9000, you must also change the "proxy" field in ./client/package.json
 var port = process.env.PORT || 9000;
 
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+const reactConfig = require(path.join(__dirname, '/config/config.static.json'))[env];
+
 var config = require('./config/config');
 
 app.use(express.static(path.join(__dirname, reactConfig))); // serving react files
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
-app.listen(port);
+io.on('connection', function (socket) {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
 
 // ****** IMPORTANT *******
 // Don't create a get route for '/' because heroku will get confused and overwrite the static react files
@@ -41,4 +38,3 @@ app.listen(port);
 //app.route('/').get(function(request, response) {
 //   response.json(config);
 // });
-
