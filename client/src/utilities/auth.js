1adcbaaa188319f8b6e266e3185ab0f15eafa079
Burke <burke1791@gmail.com>
2019-06-28 16:53:48 -0500

    added validation functions for the signin and signup requests

--- client/src/utilities/auth.js
+++ client/src/utilities/auth.js
@@ -4,47 +4,65 @@ import { API, NOTIF } from './constants';
 
 var Auth = {};
 
-(function(obj) {
+(function (obj) {
   // @TODO 
   var user = {};
 
-  obj.sendSigninRequest = (email, password) => {
-    axios.post(API.signin, {
-      email: email,
-      password: password
-    }).then(response => {
-      user = response.data.user;
-      Pubsub.publish(NOTIF.SIGN_IN, null);
-    }).catch(error => {
-      // @TODO return error codes and display helpful messages to the user, i.e. incorrect password, etc.
-      // Potentially make more DRY
-      console.log(error);
-    });
+  obj.sendSigninRequest = (params) => {
+    // API require email OR alias
+    // forcing email at the moment - may implement more elegant logic later
+    if (validateSigninRequest(params)) {
+      let signinObj = {
+        email: params.email,
+        password: params.password
+      };
+
+      axios.post(API.signin, signinObj).then(response => {
+        user = response.data.user;
+        Pubsub.publish(NOTIF.SIGN_IN, null);
+      }).catch(error => {
+        // @TODO return error codes and display helpful messages to the user, i.e. incorrect password, etc.
+        // Potentially make more DRY
+        console.log(error);
+      });
+    } else {
+      // @TODO kick back a real error and display in the modal
+      console.log('ERROR: problem with signin params:');
+      console.log(params);
+    }
   }
 
   obj.sendSignupRequest = (params) => {
-    console.log('sent signup request');
-    axios.post(API.signup, {
-      first_name: params.first_name,
-      last_name: params.last_name,
-      alias: params.alias,
-      email_address: params.email,
-      password: params.password,
-      password_confirm: params.password_confirm
-    }).then(response => {
-      user = response.data.user;
-      Pubsub.publish(NOTIF.SIGN_IN, null);
-    }).catch(error => {
-      // @TODO return error codes and display helpful messages to the user, i.e. incorrect password, etc.
-      // Potentially make more DRY
-      console.log(error);
-    });
+    if (validateSignupRequest(params)) {
+      console.log('sent signup request');
+      axios.post(API.signup, {
+        first_name: params.first_name,
+        last_name: params.last_name,
+        alias: params.alias,
+        email_address: params.email,
+        password: params.password,
+        password_confirm: params.password_confirm
+      }).then(response => {
+        user = response.data.user;
+        Pubsub.publish(NOTIF.SIGN_IN, null);
+      }).catch(error => {
+        // @TODO return error codes and display helpful messages to the user, i.e. incorrect password, etc.
+        // Potentially make more DRY
+        console.log(error);
+      });
+    } else {
+      // @TODO kick back a real error and display in the modal
+      console.log('ERROR: problem with signup params:');
+      console.log(params);
+    }
+
   }
 
   obj.sendSignoutRequest = () => {
+    // @TODO need to verify what direction we're taking with the session token business
     axios({
       url: API.signout,
-      method: 'patch',
+      method: 'delete',
       data: {
         user_id: user.user_id
       }
@@ -64,5 +82,32 @@ var Auth = {};
   // @TODO implement some sort of persistent session check
 
 })(Auth);
+
+const validateSigninRequest = (params) => {
+  // API requires either email or alias, and password
+  if ((params.alias || params.email) && params.password) {
+    return true;
+  }
+  return false;
+}
+
+const validateSignupRequest = (params) => {
+  /* API requires all of:
+    first_name
+    last_name
+    email_address
+    alias
+    password
+  */
+  if (params.first_name &&
+    params.last_name &&
+    params.email_address &&
+    params.alias &&
+    params.password) {
+    return true;
+  }
+
+  return false;
+}
 
 export default Auth;
