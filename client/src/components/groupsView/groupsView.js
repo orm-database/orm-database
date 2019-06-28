2a0b1aa5a96bbb405938df8e79a618d877f2bb73
Burke <burke1791@gmail.com>
2019-06-28 17:49:48 -0500

    added plus symbol for users to click to add a channel

--- client/src/components/groupsView/groupsView.js
+++ client/src/components/groupsView/groupsView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './groupsView.css';
+import plusSign from '../../images/plus.svg';
 
 import ChatListGroup from '../chatListGroup/chatListGroup';
 
@@ -10,11 +11,20 @@ function GroupsView(props) {
     selectedGroupId: Number - group_id of the chat group
   } */
 
+  const addChannel = () => {
+    // @TODO implement logic for adding a channel
+    console.log('add channel');
+  }
+
   // @TODO will likely need to add logic to distinguish between channels group and direct group
   return (
     <div className='col-lg-2 p-0 group-menu scrollable'>
       <div className='channels-container mt-1 mb-4'>
-        <h5 className='pl-2 text-light'>Channels</h5>
+        <div className='list-group-header d-flex justify-content-between'>
+          <h5 className='pl-2 text-light'>Channels</h5>
+          <button type='button' className='add-btn' onClick={addChannel}></button>
+        </div>
+        
         <ChatListGroup groupType={CHAT_GROUP_TYPES.group} selectedGroupId={props.selectedGroupId} />
       </div>
       
