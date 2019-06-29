7a66b714338709b62bfe0a4693ddbcf70cda7afb
justin <jlouie10@gmail.com>
2019-06-29 11:29:04 -0500

    Add POST /api/channels/:channel_id method for adding channel users to a channel

--- controllers/channels.js
+++ controllers/channels.js
@@ -4,13 +4,13 @@ var channel = require("../models/channel.js");
 
 router.get("/api/channels", (req, res) => {
     console.log('retrieve all channels');
-    channel.select( (rows) => {
+    channel.select((rows) => {
         res.json(rows);
     })
 });
 
 router.get("/api/channels/:channel_id", (req, res) => {
-    console.log('retrieve channel: '+req.params.channel_id);
+    console.log('retrieve channel: ' + req.params.channel_id);
     channel.selectWhere(req.params, (rows) => {
         res.json(rows);
     })
@@ -25,7 +25,7 @@ router.post("/api/channels", (req, res) => {
 });
 
 router.delete("/api/channels/:channel_id", (req, res) => {
-    console.log('delete channel: '+req.params.channel_id);
+    console.log('delete channel: ' + req.params.channel_id);
     console.log(req.params);
 
     channel.delete(req.params.channel_id, (result) => {
@@ -34,6 +34,32 @@ router.delete("/api/channels/:channel_id", (req, res) => {
         } else {
             res.status(200).end();
         }
+    });
+});
+
+// POST route for adding channel users
+router.post('/api/channels/:channel_id', (req, res) => {
+    console.log('add channel users');
+
+    req.body.users.forEach(userId => {
+        channel.insertChannelUsers(
+            {
+                channel_id: req.params.channel_id,
+                user_id: userId
+            },
+            (err, result) => {
+                console.log(result)
+                if (err) {
+                    console.log(err);
+
+                    res.status(500).json({ 'error': 'oops we did something bad' });
+                } else {
+                    res.status(200).json({
+                        message: 'complete'
+                    });
+                }
+            }
+        );
     });
 });
 
